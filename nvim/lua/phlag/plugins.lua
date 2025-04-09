cc951cd9de581c78e8725a02f87cc4100284c620
Jonathan Grenier <phlagmatic@gmail.com>
2025-04-09 10:34:28 -0400

    First

---
+++ nvim/lua/phlag/plugins.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'github/copilot.vim',
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ''
+      vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("<CR>")', { expr = true, silent = true, script = true, replace_keycodes = false })
+      vim.keymap.set('i', '<C-à>', 'copilot#AcceptLine()', { expr = true, silent = true, script = true, replace_keycodes = false })
+    end,
+  },
+  -- flash.nvim
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    lazy = false,
+  },
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = { enable = true },
+          shortcut = {
+            { desc = 'Find File', group = '@property', action = 'Telescope find_files', key = 'f' },
+            { desc = 'Find Word', group = '@property', action = 'Telescope live_grep', key = 'g' },
+            { desc = 'Recent Files', group = '@property', action = 'Telescope oldfiles', key = 'r' },
+            { desc = 'Settings', group = '@property', action = 'edit ~/.config/nvim/init.lua', key = 's' },
+          },
+        },
+      }
+    end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  },
+}
