2957a1e7d8a3e2a0f192d3c07110ba29b1cd4b52
Jonathan Grenier <phlagmatic@gmail.com>
2025-04-22 09:30:01 -0400

    Update dotfiles: 2025-04-22 09:30:01

--- nvim/init.lua
+++ nvim/init.lua
@@ -104,6 +104,8 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 vim.keymap.set('n', '<C-s>', '<cmd>:w<CR>', { desc = 'Save file' })
+-- open oil.nvim
+vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'Open Oil.nvim' })
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
